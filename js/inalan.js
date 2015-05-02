0c0bedd85b1af043194ff180f99c354598b6227e
Ladislav Végh <vegh34@gmail.com>
2015-05-02 18:36:19 +0200

    added inalan.js file

---
+++ js/inalan.js
@@ -0,0 +1,16 @@
+﻿// Namespace:
+var inalan = inalan || {};
+
+// ***** Stage class *****
+inalan.Stage = function (canvasId) {
+
+}
+
+inalan.Stage.prototype.update = function () {
+
+}
+
+inalan.Stage.prototype.addArray = function () {
+
+}
+
