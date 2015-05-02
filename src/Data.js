b4169699cb470b4c88e26985c38c01399b2640f9
Ladislav Végh <vegh34@gmail.com>
2015-05-02 19:03:03 +0200

    added Stage class

---
+++ src/Data.js
@@ -0,0 +1,19 @@
+﻿/*
+*
+* Copyright (c) 2015- Ladislav Végh, Komárno, Slovakia
+*
+* Distributed under the terms of the MIT license.
+* http://www.opensource.org/licenses/mit-license.html
+*
+* This notice shall be included in all copies or substantial portions of the Software.
+*
+*/
+
+var inalan = inalan || {};
+
+inalan.Data = function (name) {
+    this.x = 0;
+    this.y = 0;
+    this.name = name;
+}
+
---
+++ src/Stage.js
@@ -0,0 +1,25 @@
+﻿/*
+*
+* Copyright (c) 2015- Ladislav Végh, Komárno, Slovakia
+*
+* Distributed under the terms of the MIT license.
+* http://www.opensource.org/licenses/mit-license.html
+*
+* This notice shall be included in all copies or substantial portions of the Software.
+*
+*/
+
+var inalan = inalan || {};
+
+inalan.Stage = function (canvasId) {
+	this.canvas = document.getElementById("myCanvas").getContext("2d");
+}
+
+inalan.Stage.prototype.update = function () {
+
+}
+
+inalan.Stage.prototype.add = function (data) {
+
+}
+
