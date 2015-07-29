46a99e71e046d7b740c681aa3849a133fc5d484d
Ladislav Végh <vegh34@gmail.com>
2015-07-29 22:04:58 +0200

    minor fix

--- src/VisuArray.js
+++ src/VisuArray.js
@@ -362,5 +362,6 @@ inalan.VisuArray.prototype.setMaxValue = function (value) {
 inalan.VisuArray.prototype.setHeight = function (height) {
     for (var i = 0; i < this.length; i++) {
         this[i].height = height;
+        this[i].maxValue = height;
     }
 }
